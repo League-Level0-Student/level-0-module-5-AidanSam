8c2533467299f4082e6413ef03520b9e16b254f0
root <root@383847912c98>
2020-05-07 02:59:30 +0000

    I worked on stuff

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -19,42 +19,46 @@ notarobot.moveTo(300, 300);
 notarobot.penDown();
 
 		// #4. Set the robot’s speed to 5
-notarobot.setSpeed(5);
-
-
+notarobot.setSpeed(10);
+notarobot.hide();
+for(int j = 1; j<7; j++) {
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
 for(int i = 1; i<7; i++) {
 	
 
 			
 			// #6. Set the pen color to black
-notarobot.setPenColor(color);
+notarobot.setPenColor(Color.black);
 			
 			// #7. move the robot by 50
-notarobot.move(distance);
+notarobot.move(50);
 			
 			// #8. turn the robot to the right by 60
-			
+			notarobot.turn(60);
 			
 			// #12. set the pen color to blue for even numbers of i
-
+if(i%2 == 0) {
+	notarobot.setPenColor(Color.blue);
+}else {
+	notarobot.setPenColor(Color.red);
+}
 			
 			// #13. set the pen color to red for all odd numbers of i
-			
-			
+
+}
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
 			
 			
 					// #10. turn the robot to the left by 60
-		
+		notarobot.turn(60);
 					
 					// #11. move the robot by 50 (play the start button)
-					
+					notarobot.move(50);
 				
 			
 			// #14. set the pen color to black
-	notarobot.setPenColor(color);
-}				
+	notarobot.setPenColor(Color.black);
+}			
 		
 	}
 
---
+++ src/_03_method_writing/_1_obedient_robot/robot.java
@@ -0,0 +1,39 @@
+package _03_method_writing._1_obedient_robot;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class robot {
+		Robot notarobot = new Robot();
+void ask() {
+
+	notarobot.setSpeed(500);
+	notarobot.hide();
+	notarobot.penDown();
+	String shape = JOptionPane.showInputDialog("Circle, square, or triangle?");
+	if(shape.equalsIgnoreCase("circle")) {
+		circle();
+	}else if(shape.equalsIgnoreCase("square")){
+		square();
+	}else if(shape.equalsIgnoreCase("triangle")) {
+		triangle();
+	}
+}
+void circle() {
+	for(int i=0; i<360; i++) {
+		notarobot.move(1);
+		notarobot.turn(1);
+	}
+}
+void square() {
+	for(int j=0; j<4; j++) {
+		notarobot.move(50);
+		notarobot.turn(1);
+	}
+}
+void triangle() {
+	
+}
+}
+
